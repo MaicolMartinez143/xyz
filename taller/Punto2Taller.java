e4b2ab4b1a33d42941a18d21296b9af6771f7225
MaicolMartinez143 <maicolmartinez124@unisangil.edu.co>
2024-09-16 20:49:55 -0500

    Add files via upload

---
+++ taller/Punto2Taller.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package punto.pkg2.taller;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Punto2Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
